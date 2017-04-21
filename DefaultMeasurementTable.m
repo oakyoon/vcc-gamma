e1361c8d909c74475ec3197c7653c7bf0b74b411
Oakyoon Cha <oakyoon@gmail.com>
2017-04-21 22:02:25 +0900

    default profile updated to sRGB

--- DefaultMeasurementTable.m
+++ DefaultMeasurementTable.m
@@ -1,11 +1,6 @@
-function mtable = DefaultMeasurementTable(gamma)
-	if ~exist('gamma', 'var') || isempty(gamma)
-		gamma = 2.2;
-	end
-
+function mtable = DefaultMeasurementTable()
 	mtable = zeros(256, 7);
 	mtable(:, 1)   = (0:255)';
 	mtable(:, 2:4) = LinearGammaTable;
-	gamma_lums = (linspace(0, 1, 256)' .^ gamma) * 100;
-	mtable(:, 5:7) = repmat(gamma_lums, 1, 3) .* repmat(LumRatio_RGB, 256, 1);
+	mtable(:, 5:7) = (TargetLum_sRGB() * 100) .* repmat(LumRatio_RGB, 256, 1);
 end
