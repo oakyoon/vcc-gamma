359af297a385b8ac68275c17c3f4ab752c54ccc7
Oakyoon Cha <oakyoon@gmail.com>
2018-04-13 20:28:25 -0500

    added argument default

--- RelLum_Measured.m
+++ RelLum_Measured.m
@@ -1,3 +1,7 @@
 function rel_lum = RelLum_Measured(mtable)
+	if ~exist('mtable', 'var') || isempty(mtable)
+		mtable = LoadMeasurementTable;
+	end
+
 	rel_lum = mtable(end, 5:7) ./ sum(mtable(end, 5:7));
 end
